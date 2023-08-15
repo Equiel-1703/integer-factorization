782405a91c37a347c9872590046de748af2f31b6
Henrique Rodrigues <96885946+Equiel-1703@users.noreply.github.com>
2023-08-14 21:43:40 -0300

    implemented multithread and changed prime generator

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
+use std::collections::LinkedList;
 use std::env;
+use std::ops::Add;
+use std::ops::Div;
+use std::thread;
+use std::sync::mpsc;
+use std::usize;
+use num_bigint::BigUint;
+use rand::Rng;
 
 /*
 
@@ -7,60 +15,85 @@ Note: May have to use bigger data types to support our targer num (384 bits - at
 BigUint may do the trick:
 https://stackoverflow.com/questions/50504503/is-there-a-limit-to-the-size-of-a-bigint-or-biguint-in-rust
 
-For now, I'm just going to use i32
-
 */
 
+// Probabilistic test for primality
+static K: i32 = 5;
+fn fermat_test(num: BigUint) -> bool {
+    if num < BigUint::from(2 as usize) {
+        return false;
+    } else if num == BigUint::from(2 as usize) || num == BigUint::from(3 as usize) || num == BigUint::from(5 as usize) {
+        return true;
+    } 
+
+    // Generate random number
+    let a: BigUint = BigUint::from(rand::thread_rng().gen_range(2..u128::MAX));
+
+    for _ in 0..K {
+        if a.modpow(&(num.clone() - BigUint::from(1 as usize)), &num) != BigUint::from(1 as usize) {
+            return false;
+        }
+    }
+
+    true
+}
+
 fn main() {
-    let until = env::args().collect::<Vec<String>>()[1].parse::<i32>().unwrap();
-    print!("Calculating primes up to {}\n", until);
+    // Input format: nf <nthreads> <num>
+    let nthreads = env::args().collect::<Vec<String>>()[1].parse::<i32>().unwrap();
+    let num = env::args().collect::<Vec<String>>()[2].parse::<BigUint>().unwrap();
+    
+    println!("Number of threads: {}", nthreads);
+    println!("Number to factor: {}\n", num);
 
-    // This code is meant to be as fast as possible, to do so, we will use lot of contiguos memory (Vec).
-    // This consumes more memory, but it's faster than using linked lists, since we cannot access 
-    // elements in given index in constant time (O(n) instead of O(1))
+    // Testing multithreading in Rust
+    let increment = (num.sqrt()).div(nthreads as usize);
+    let mut start_point = BigUint::from(0 as usize);
+    let mut end_point = increment.clone();
 
-    let mut nums: Vec<i32> = Vec::with_capacity((until - 1) as usize);
-    let mut primes: Vec<i32> = Vec::with_capacity((until - 1) as usize);
+    println!("Increment: {}", increment);
+    println!("Start point: {}", start_point);
+    println!("End point: {}\n\n", end_point);
 
-    print!("Allocated {} positions for arrays\n", nums.capacity());
+    let (tx, rx) = mpsc::channel(); 
 
+    for _ in 0..nthreads {
+        let tx_clone = tx.clone();
+        let st_clone = start_point.clone();
+        let en_clone = end_point.clone();
+        let num_clone = num.clone();
 
-    // Create list with all numbers from 2 to until
-    for i in 2..=until {
-        nums.push(i);
+        println!("Start point: {}", st_clone);
+        println!("End point: {}", en_clone);
+
+        thread::spawn(move || {
+            let mut factors: LinkedList<BigUint> = LinkedList::new();
+
+            let mut s = st_clone;
+            let e = en_clone;
+
+            while s <= e {
+                if fermat_test(s.clone()) &&
+                    num_clone.modpow(&BigUint::from(1 as usize), &s) == BigUint::from(0 as usize){
+                    factors.push_back(s.clone());
+                }
+                s = s.add(BigUint::from(1 as usize));
+            }
+            tx_clone.send(factors).unwrap();
+        });
+
+        start_point += increment.clone();
+        end_point += increment.clone();
     }
 
-    print!("Nums: {:?}\n", nums);
+    drop(tx);
 
-    // Remove non-primes (sieve of Eratosthenes)
-    let mut i: usize = 0;
-    loop {
-        if i >= nums.len() {
-            break;
-        }
+    let it = rx.iter();
+    let mut final_primes: LinkedList<BigUint> = LinkedList::new();
+    it.for_each(|a| {
+        final_primes.append(&mut a.clone());
+    });
 
-        let inc = nums.get(i).unwrap().clone();
-        
-        // Not prime
-        if inc == 0 {
-            i += 1;
-            continue;
-        }
-
-        // Prime
-        primes.push(inc);
-
-        // Remove all multiples of this prime
-        let mut temp = i + inc as usize;
-        while temp < nums.len() {
-            nums[temp] = 0;
-            temp += inc as usize;
-        }
-
-        i += 1;
-    }
-
-    print!("Primes: {:?}\n", primes);
-    primes.shrink_to_fit();
-
+    print!("\n{:?}", final_primes);
+    
 }
